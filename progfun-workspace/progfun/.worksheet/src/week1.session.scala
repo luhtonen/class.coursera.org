8b535242c70f85e3c3d30c14ec6f4e5a8950654f
Eduard Luhtonen <luhtonen@gmail.com>
2014-09-20 11:53:40 +0400

    removing redundant arguments

--- progfun-workspace/progfun/.worksheet/src/week1.session.scala
+++ progfun-workspace/progfun/.worksheet/src/week1.session.scala
@@ -2,23 +2,23 @@ package week1
 
 object session {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(39); val res$0 = 
   1 + 3;System.out.println("""res0: Int(4) = """ + $show(res$0));$skip(44); 
-  def abs(x: Double) = if (x < 0) -x else x;System.out.println("""abs: (x: Double)Double""");$skip(378); 
+  def abs(x: Double) = if (x < 0) -x else x;System.out.println("""abs: (x: Double)Double""");$skip(333); 
 
   def sqrt(x: Double) = {
-    def sqrtIter(guess: Double, x: Double): Double = {
-      if (isGoodEnough(guess, x)) guess
-      else sqrtIter(improve(guess, x), x)
+    def sqrtIter(guess: Double): Double = {
+      if (isGoodEnough(guess)) guess
+      else sqrtIter(improve(guess))
     }
 
-    def isGoodEnough(guess: Double, x: Double) = {
+    def isGoodEnough(guess: Double) = {
       abs(guess * guess - x) / x < 0.001
     }
 
-    def improve(guess: Double, x: Double) = {
+    def improve(guess: Double) = {
       (guess + x / guess) / 2
     }
 
-    sqrtIter(1.0, x)
+    sqrtIter(1.0)
   };System.out.println("""sqrt: (x: Double)Double""");$skip(11); val res$1 = 
 
   sqrt(2);System.out.println("""res1: Double = """ + $show(res$1));$skip(10); val res$2 = 
--- progfun-workspace/progfun/src/week1/session.sc
+++ progfun-workspace/progfun/src/week1/session.sc
@@ -5,20 +5,20 @@ object session {
   def abs(x: Double) = if (x < 0) -x else x       //> abs: (x: Double)Double
 
   def sqrt(x: Double) = {
-    def sqrtIter(guess: Double, x: Double): Double = {
-      if (isGoodEnough(guess, x)) guess
-      else sqrtIter(improve(guess, x), x)
+    def sqrtIter(guess: Double): Double = {
+      if (isGoodEnough(guess)) guess
+      else sqrtIter(improve(guess))
     }
 
-    def isGoodEnough(guess: Double, x: Double) = {
+    def isGoodEnough(guess: Double) = {
       abs(guess * guess - x) / x < 0.001
     }
 
-    def improve(guess: Double, x: Double) = {
+    def improve(guess: Double) = {
       (guess + x / guess) / 2
     }
 
-    sqrtIter(1.0, x)
+    sqrtIter(1.0)
   }                                               //> sqrt: (x: Double)Double
 
   sqrt(2)                                         //> res1: Double = 1.4142156862745097
