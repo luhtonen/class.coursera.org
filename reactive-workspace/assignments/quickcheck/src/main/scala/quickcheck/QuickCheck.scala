de6114868539254544789f77fc08edcaf3519888
luhtonen <luhtonen@gmail.com>
2015-04-21 09:07:04 +0200

    Assignment for week 1 - added some properties

--- reactive-workspace/assignments/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ reactive-workspace/assignments/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -14,7 +14,39 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
-  lazy val genHeap: Gen[H] = ???
+  property("gen1") = forAll {(h: H) =>
+    val m = if(isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h)) == m
+  }
+
+  property("min2") = forAll { (a: Int, b: Int) =>
+    val h = insert(a, empty)
+    findMin(insert(b, h)) == math.min(a, b)
+  }
+
+  property("del1") = forAll { a: Int =>
+    deleteMin(insert(a, empty)) == empty
+  }
+
+  property("meld1") = forAll { (h1: H, h2: H) =>
+    findMin(meld(h1, h2)) == math.min(findMin(h1), findMin(h2))
+  }
+
+  def deleteMinima(xs: Seq, h: H): (Seq, H) = {
+    xs = xs :+ findMin(h)
+    (xs, h) = deleteMinima(xs, deleteMin(h))
+  }
+
+  property("del2") = forAll { (h: H) =>
+    val xs = new Seq
+    (xs, h) = deleteMinima(xs, h)
+    xs == xs.sorted
+  }
+
+  lazy val genHeap: Gen[H] = for {
+    v <- arbitrary[Int]
+    h <- oneOf(const(empty), genHeap)
+  } yield insert(v, h)
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
